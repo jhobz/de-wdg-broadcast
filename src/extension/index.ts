cff9c72c55e30b47392d203abeab4b065d079324
Jeff Hobson <jeffrey296@gmail.com>
2024-02-22 11:26:59 -0500

    Add ability to read from Google Sheets

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,21 +1,40 @@
-import type NodeCG from '@nodecg/types';
-import type { ExampleReplicant } from '../types/schemas';
+import type NodeCG from '@nodecg/types'
+import type { ExampleReplicant } from '../types/schemas'
+
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { JWT } from 'google-auth-library'
+import creds  from './../../.config/wizards-district-gaming-13fa1a1ef6e2.json'
 
 module.exports = function (nodecg: NodeCG.ServerAPI) {
-	nodecg.log.info("Hello, from your bundle's extension!");
-	nodecg.log.info("I'm where you put all your server-side code.");
-	nodecg.log.info(
-		`To edit me, open "${__filename.replace(
-			'build/extension',
-			'src/extension',
-		)}" in your favorite text editor or IDE.`,
-	);
-	nodecg.log.info('You can use any libraries, frameworks, and tools you want. There are no limits.');
-	nodecg.log.info('Visit https://nodecg.dev for full documentation.');
-	nodecg.log.info('Good luck!');
+	nodecg.log.info("Hello, from your bundle's extension!")
 
-	const exampleReplicant = nodecg.Replicant('exampleReplicant') as unknown as NodeCG.ServerReplicantWithSchemaDefault<ExampleReplicant>;
+	const exampleReplicant = nodecg.Replicant('exampleReplicant') as unknown as NodeCG.ServerReplicantWithSchemaDefault<ExampleReplicant>
 	setInterval(() => {
-		exampleReplicant.value.age++;
-	}, 5000);
-};
+		exampleReplicant.value.age++
+	}, 5000)
+
+	authorizeGoogle(nodecg)
+}
+
+async function authorizeGoogle(nodecg: NodeCG.ServerAPI) {
+	const serviceAccountAuth = new JWT({
+		// email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+		// key: process.env.GOOGLE_PRIVATE_KEY,
+		email: creds.client_email,
+		key: creds.private_key,
+		scopes: [
+			'https://www.googleapis.com/auth/spreadsheets',
+		],
+	})
+
+	const doc = new GoogleSpreadsheet('1je1brcelW1SDgeuTFsS9ulsyiuNlfe5trZndqDd9kfQ', serviceAccountAuth)
+
+	await doc.loadInfo()
+	nodecg.log.info(doc.title)
+	// await doc.updateProperties({ title: 'renamed doc' })
+
+
+	const sheet = doc.sheetsByIndex[0]
+	nodecg.log.info(sheet.title)
+	nodecg.log.info(sheet.rowCount)
+}
