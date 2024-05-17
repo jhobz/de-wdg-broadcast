895a20554259636b88e2be2cae5c28b88a30a3a0
Jeff Hobson <jeffrey296@gmail.com>
2024-05-17 18:12:02 -0400

    Style UpcomingGames graphic

--- src/graphics/UpcomingGames.tsx
+++ src/graphics/UpcomingGames.tsx
@@ -4,9 +4,41 @@ import { useReplicant } from '@nodecg/react-hooks'
 import { AssetOverlay } from '../components/AssetOverlay'
 import { Schedule } from '../types/schemas'
 import { FlexColumn } from '../components/layout/Flexbox'
+import styled from 'styled-components'
+import { TeamLogo } from '../components/TeamLogo'
 
 const SPECIAL_ASSET_NAME = 'upcoming-games'
 const NUMBER_OF_GAMES_TO_DISPLAY = 3
+
+const MatchColumn = styled(FlexColumn)`
+    width: 888px;
+    height: 1080px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 20px 180px 20px 214px; // Damn you, Cam!
+    color: white;
+    text-shadow: 5px 5px 5px #000;
+    -webkit-text-stroke: 2px black;
+`
+
+const MatchRow = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 194px);
+    grid-template-rows: 182px 104px;
+    column-gap: 106px;
+    row-gap: 32px;
+    text-align: center;
+    align-items: center;
+`
+
+const MatchImage = styled(TeamLogo)`
+    width: 100%;
+    height: 100%;
+    padding: 0 12px;
+    object-fit: contain;
+    filter: drop-shadow(2px 2px 4px #000);
+`
 
 export const UpcomingGames: React.FC = () => {
     // @ts-expect-error This is an error with useReplicant that will be fixed in the next version
@@ -26,12 +58,30 @@ export const UpcomingGames: React.FC = () => {
             )
         })
         .map((match, i) => {
-            return <div key={i}>{`${match.opponent} on ${match.date}`}</div>
+            const date = new Date(match.date).toLocaleDateString(['en-US'], {
+                month: 'numeric',
+                day: 'numeric',
+            })
+            const time = new Date(match.date)
+                .toLocaleTimeString(['en-US'], {
+                    hour: 'numeric',
+                    minute: '2-digit',
+                })
+                .replace(' AM', '')
+                .replace(' PM', '')
+            return (
+                <MatchRow key={i}>
+                    <div></div>
+                    <MatchImage team={match.opponent}></MatchImage>
+                    <div>{date}</div>
+                    <div>{time}</div>
+                </MatchRow>
+            )
         })
 
     return (
         <AssetOverlay assetName={SPECIAL_ASSET_NAME}>
-            <FlexColumn>{matchElements}</FlexColumn>
+            <MatchColumn gap="43px">{matchElements}</MatchColumn>
         </AssetOverlay>
     )
 }
